7267527ae845a48ad35dd751d7687374decf7d3e
tai2319 <99863150+tai2319@users.noreply.github.com>
2023-11-16 13:56:25 +0700

    tài

--- DATN/src/main/java/com/ps20652/DATN/controller/IndexController.java
+++ DATN/src/main/java/com/ps20652/DATN/controller/IndexController.java
@@ -1,6 +1,5 @@
 package com.ps20652.DATN.controller;
 
-
 import java.io.IOException;
 
 import java.nio.file.Files;
@@ -64,18 +63,18 @@ public class IndexController {
     private AccountService accountService;
     @Autowired
     private OrderDetailService orderDetailService;
-//    @Autowired
-//    private ReviewReplyService replyService;
-//    
+    // @Autowired
+    // private ReviewReplyService replyService;
+    //
 
     @GetMapping
-    public String listProducts(Model model, Authentication authentication,  @RequestParam(name = "confirmationMessage", required = false) String confirmationMessage) {
+    public String listProducts(Model model, Authentication authentication,
+            @RequestParam(name = "confirmationMessage", required = false) String confirmationMessage) {
         List<CustomerFeedback> feedback = feedbackService.findAll();
         List<Product> products = productService.findAll();
         List<Category> cat = categoryService.findAll();
         Map<Integer, Integer> unitsSoldMap = new HashMap<>();
         Map<Integer, List<Product>> topProductsPerCategory = new HashMap<>();
-
 
         if (authentication != null && authentication.isAuthenticated()) {
             String username = authentication.getName();
@@ -83,8 +82,7 @@ public class IndexController {
 
             // Lấy userID của người dùng đã đăng nhập
             int userId = getUserIDByUsername(username);
-            
-            
+
             // Lấy số lượng sản phẩm trong giỏ hàng
             int cartItemCount = cartService.getCount(userId);
             model.addAttribute("cartItemCount", cartItemCount);
@@ -94,21 +92,23 @@ public class IndexController {
             int unitsSold = orderDetailService.getProductSell(product);
             unitsSoldMap.put(product.getProductId(), unitsSold);
         }
-        
-//        for (Category category : cat) {
-//            List<Product> topProducts = productService.getTop4BestSellingProductsPerCategory(category.getCategoryId());
-//            model.addAttribute("topProductsPerCategory_" + category.getCategoryId(), topProducts);
-//        }
-        
+
+        // for (Category category : cat) {
+        // List<Product> topProducts =
+        // productService.getTop4BestSellingProductsPerCategory(category.getCategoryId());
+        // model.addAttribute("topProductsPerCategory_" + category.getCategoryId(),
+        // topProducts);
+        // }
+
         for (Category category : cat) {
             List<Product> topProducts = productService.getTop4BestSellingProductsPerCategory(category.getCategoryId());
             topProductsPerCategory.put((category.getCategoryId()), topProducts);
         }
-        
+
         if (confirmationMessage != null) {
             model.addAttribute("confirmationMessage", confirmationMessage);
         }
-        
+
         model.addAttribute("topProductsPerCategory", topProductsPerCategory);
         model.addAttribute("unitsSoldMap", unitsSoldMap);
         model.addAttribute("products", products);
@@ -118,77 +118,89 @@ public class IndexController {
         return "user2/index2";
     }
 
-    
     @RequestMapping("/login")
     public String login() {
         return "security2/login2";
     }
+
+    @RequestMapping("/new")
+    public String New() {
+        return "main/New";
+    }
+
+    @RequestMapping("/about")
+    public String About() {
+        return "main/About";
+    }
+
     @GetMapping("/access-denied")
     public String accessDenied() {
         return "redirect:/"; // Trang thông báo lỗi truy cập không được phép
     }
-    
+
     @GetMapping("/ProductDetails/{productId}")
     public String IDProducts(@PathVariable("productId") Integer productId, Model model, Principal principal) {
         Product product = productService.findbyId(productId);
         List<CustomerFeedback> feedback = feedbackService.findByProductProductId(productId);
-        
+
         if (principal != null) {
             String username = principal.getName();
             int userId = getUserIDByUsername(username);
             model.addAttribute("userId", userId);
         }
-        
+
         model.addAttribute("products", product);
         model.addAttribute("feedbacks", feedback);
 
         return "user2/product-details";
     }
 
-    
-//    @GetMapping ("/ProductDetails/{productId}")
-//    public String IDProducts(@PathVariable("productId") Integer productId,Model model, @RequestParam Integer feedbackId ) {
-//    	  Product product = productService.findbyId(productId);
-//    	  List <CustomerFeedback> feedback = feedbackService.findByProductProductId(productId);
-//    	  List <ReviewReply> rep = replyService.findByFeedbackId(feedbackId);
-//        model.addAttribute("products", product);
-//        model.addAttribute("reply", rep);
-//        model.addAttribute("feedbacks", feedback);
-//        return "product-details";
-//    }
-    
+    // @GetMapping ("/ProductDetails/{productId}")
+    // public String IDProducts(@PathVariable("productId") Integer productId,Model
+    // model, @RequestParam Integer feedbackId ) {
+    // Product product = productService.findbyId(productId);
+    // List <CustomerFeedback> feedback =
+    // feedbackService.findByProductProductId(productId);
+    // List <ReviewReply> rep = replyService.findByFeedbackId(feedbackId);
+    // model.addAttribute("products", product);
+    // model.addAttribute("reply", rep);
+    // model.addAttribute("feedbacks", feedback);
+    // return "product-details";
+    // }
+
     @GetMapping("/searchName")
     public String searchProductsNAME(@RequestParam("name") String productName, Model model) {
         // Sử dụng phương thức tìm kiếm theo tên từ ProductDAO
         // Đây là ví dụ tìm kiếm theo tên sản phẩm
         List<Product> searchResults = productService.findByName(productName);
-        
+
         model.addAttribute("products", searchResults);
-        
+
         return "user2/index2"; // Trả về view để hiển thị kết quả tìm kiếm
     }
-    @GetMapping("/searchPrice")
-	public String price(HttpServletRequest request, Model model, @RequestParam("min")double min,
-			@RequestParam("max")double max) {					
-		//List<Product> items = dao.findByPriceBetween(min, max);
 
-		List<Product> searchResults = productService.findByPrice(min, max);
-		model.addAttribute("products", searchResults);
-     
-     return "user2/index2"; // Trả về view để hiển thị kết quả lọc
- }
-    
-    @GetMapping ("/product/category/{categoryId}")
-    public String productCategory(@PathVariable("categoryId") Integer categoryId,Model model) {
-    	List<Category> allcat = categoryService.findAll();
-    	  Category cat = categoryService.findbyId(categoryId);
-    	  List<Product> products = productService.findByCategoryCategoryId(categoryId);
+    @GetMapping("/searchPrice")
+    public String price(HttpServletRequest request, Model model, @RequestParam("min") double min,
+            @RequestParam("max") double max) {
+        // List<Product> items = dao.findByPriceBetween(min, max);
+
+        List<Product> searchResults = productService.findByPrice(min, max);
+        model.addAttribute("products", searchResults);
+
+        return "user2/index2"; // Trả về view để hiển thị kết quả lọc
+    }
+
+    @GetMapping("/product/category/{categoryId}")
+    public String productCategory(@PathVariable("categoryId") Integer categoryId, Model model) {
+        List<Category> allcat = categoryService.findAll();
+        Category cat = categoryService.findbyId(categoryId);
+        List<Product> products = productService.findByCategoryCategoryId(categoryId);
         model.addAttribute("categories", cat);
         model.addAttribute("products", products);
         model.addAttribute("allcategory", allcat);
         return "user2/product-category";
     }
-    
+
     private int getUserIDByUsername(String username) {
         // Sử dụng Spring Data JPA để truy vấn cơ sở dữ liệu
         Account user = accountService.findByUsername(username);
@@ -200,10 +212,4 @@ public class IndexController {
         return -1; // Trường hợp không tìm thấy user
     }
 
-   
-
 }
-
-
-	 
-
