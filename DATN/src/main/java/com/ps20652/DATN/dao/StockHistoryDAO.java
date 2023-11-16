32f14807495d6233c22d2208058bcb994b885ed0
six2provip <nguyengiakhang060202@gmail.com>
2023-11-16 12:51:56 +0700

    Revert "Phuc"
    This reverts commit 615b928c7216f30c0a9cb851ec386a4dce683cb1.

--- DATN/src/main/java/com/ps20652/DATN/dao/StockHistoryDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ps20652.DATN.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ps20652.DATN.entity.StockHistory;
-
-public interface StockHistoryDAO extends JpaRepository<StockHistory, Integer> {
- 
-}
